9a46b11b7df0d1fc6816e4e36b8e3130949f45ec
guijiangheng <kakaxizhh@gmail.com>
2020-08-12 08:37:52 +0800

    配置swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Blog App')
+    .setDescription('Blog App Api Spec')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
